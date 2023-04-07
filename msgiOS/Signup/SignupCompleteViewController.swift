3adbd345f2ffc5ca318c5dbbd8c625d12551ddf3
uuuunseo <jdbstjoo@gmail.com>
2023-04-08 00:02:53 +0900

    SignupCompleteViewController 퍼블리싱, LoginViewController로 이동하는 기능 추가

--- msgiOS/Signup/SignupCompleteViewController.swift
+++ msgiOS/Signup/SignupCompleteViewController.swift
@@ -2,5 +2,65 @@ import Foundation
 import UIKit
 
 class SignupCompleteViewController: BaseViewController {
+    lazy var chevronBackBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: self, action: nil).then{
+        $0.tintColor = .black
+    }
     
+    var goLoginButton = DotoriButton().then{
+        $0.setTitle("로그인 하러가기", for: .normal)
+        $0.backgroundColor = UIColor(named: "ButtonColor")
+    }
+    
+    let completeTitleLabel = UILabel().then{
+        $0.text = "완료"
+        $0.font = UIFont.boldSystemFont(ofSize: 32)
+    }
+    
+    let completeSubTitleLabel = UILabel().then{
+        $0.text = "회원가입이 완료되었습니다 \n 로그인을 이어서 진행해주세요!"
+        $0.textColor = .gray
+        $0.font = UIFont.systemFont(ofSize: 16)
+        $0.numberOfLines = 2
+        $0.textAlignment = .center
+    }
+    
+    
+    @objc func tapGoLoginButton() {
+        let login = LoginViewController()
+        navigationController?.pushViewController(login, animated: true)
+    }
+    
+    override func setup() {
+        self.view.backgroundColor = .white
+        self.navigationItem.title = "회원가입"
+        self.navigationItem.backBarButtonItem = chevronBackBarButtonItem
+    }
+    
+    override func addView() {
+        view.addSubview(goLoginButton)
+        view.addSubview(completeTitleLabel)
+        view.addSubview(completeSubTitleLabel)
+    }
+    
+    override func addTarget() {
+        goLoginButton.addTarget(self, action: #selector(tapGoLoginButton), for: .touchUpInside)
+    }
+    
+    override func location() {
+        goLoginButton.snp.makeConstraints{
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(24)
+            $0.height.equalTo(52)
+        }
+        
+        completeSubTitleLabel.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
+        completeTitleLabel.snp.makeConstraints{
+            $0.bottom.equalTo(completeSubTitleLabel.snp.top).inset(-30)
+            $0.centerX.equalToSuperview()
+        }
+    }
 }
